a3b40f496ebddacad442375c5559224ebbfcf67b
Andrew Hayward <github@andrewhayward.net>
2012-11-26 16:41:13 +0000

    Adding basic view for make resource

--- MakeAPI/makes/urls.py
+++ MakeAPI/makes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name='makes_home'),
+    url(r'^(?P<id>\w+(-\w+){4})$', views.resource, name='makes_resource'),
 )
--- MakeAPI/makes/views.py
+++ MakeAPI/makes/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.core import serializers
+from django.utils import simplejson
+from MakeAPI.makes.models import Make
 
 
 def home (request):
     return HttpResponse("MakeAPI.makes")
+
+
+def resource (request, id):
+    make = get_object_or_404(Make, id=id)
+    return HttpResponse("MakeAPI.makes.resource::%s" % make)
