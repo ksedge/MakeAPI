31f5d5993decb95828c8a714902165826758cedf
Andrew Hayward <github@andrewhayward.net>
2012-11-26 16:36:51 +0000

    Basic fields for Make model

--- MakeAPI/makes/models.py
+++ MakeAPI/makes/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from MakeAPI.db.fields import UUIDField
 
-# Create your models here.
+
+class Make (models.Model):
+
+    id = UUIDField(version=4, primary_key=True, verbose_name='ID')
+    version = UUIDField(version=4, auto_update=True)
+    url = models.URLField(verbose_name='URL')
+    content_type = models.CharField(max_length=255, verbose_name='Content Type')
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__ (self):
+        return self.id
+
+    @property
+    def published (self):
+        return self.published_at is not None
+
+    @property
+    def deleted (self):
+        return self.deleted_at is not None
+
+    @models.permalink
+    def get_absolute_url (self):
+        return ('makes_resource', (), {
+            'id': self.id,
+        })
